2cdef88337dabff5b45312af081805b07939fb4a
Sminseo10818 <129276998+Sminseo10818@users.noreply.github.com>
2023-08-02 22:03:20 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Player
+{
+    int EXP = 0;
+    int AP = 100;
+    int MP = 200;
+    int HP = 10;
+
+}
+class Enemy
+{
+    int AP = 100;
+    int MP = 200;
+    int HP = 10;
+}
+
+namespace _23inheritance
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
